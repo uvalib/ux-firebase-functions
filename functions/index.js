3a2f9d2aab65edbe7dbd9722872d978bbd728ee7
jlk4p <jlk4p@virginia.edu>
2018-08-13 17:31:06 -0400

    save code clean up; errors appearing in firebase logs

--- functions/index.js
+++ functions/index.js
@@ -19,12 +19,16 @@ const mailTransport = nodemailer.createTransport({
 
 // Process each form request that gets submitted.
 exports.processRequest = functions.database.ref('/requests/{requestId}').onCreate((snapshot, context) => {
+    // Document the request id for possible use in logging errors.
+    const requestId = context.params.requestId;
     // Grab the request.
-    let req = JSON.parse(snapshot.submission.val());
-    let when = snapshot.timestamp.val();
+    const newRequest = snapshot.val();
+    console.log(newRequest);
+    const reqDetails = JSON.parse(newRequest.submission);
+    const when = new Date(newRequest.timestamp);
     let results = Array();
-    console.log('req: ' + req);
-    console.log('when: ' + when);
+    console.log(`details: ${reqDetails.toString()}`);
+    console.log(`when: ${when.toString()}`);
 
     // @TODO Validation of required inputs before we consider anything and redirect back to form???
     // @TODO Just thinking... submit click in the form should make sure all required fields have been populated at least!
@@ -41,19 +45,21 @@ exports.processRequest = functions.database.ref('/requests/{requestId}').onCreat
     let patronOptions = {
         from: '"UVA Library" <NO-REPLY-LIBRARY@Virginia.EDU>',
         replyTo: '"UVA Library" <NO-REPLY-LIBRARY@Virginia.EDU>',
-        to: req.fld_email_address,
+        to: '',
         subject: '',
         text: '',
         html: ''
     };
 
     // Identify the request type and process...
-    switch (req.form_id) {
+    switch (reqDetails.form_id) {
         case 'purchase_requests':
-            results = prepPurchaseRequestInfo(req, libraryOptions, patronOptions);
+            console.log('purchase request');
+            results = prepPurchaseRequestInfo(reqDetails, libraryOptions, patronOptions);
             break;
         case 'government_information_contact_u':
-            results = prepGovernmentInformationRequest(req, libraryOptions, patronOptions);
+            console.log('gov dogs request');
+            results = prepGovernmentInformationRequest(reqDetails, libraryOptions, patronOptions);
             break;
         default:
             results = [];
@@ -64,46 +70,32 @@ exports.processRequest = functions.database.ref('/requests/{requestId}').onCreat
     if (results.length > 0) {
         // If the form submission is valid then continue...
         if (results['valid']) {
-            const tasks = [];
-            tasks.push(mailTransport.sendEmail(results['libEmailOptions']));
-            tasks.push(mailTransport.sendEmail(results['patronEmailOptions']));
-            tasks.push(saveToLibInsight['']);
-            return Promise.all(tasks);
+            /*            const tasks = [];
+                        tasks.push(mailTransport.sendEmail(results['libEmailOptions']));
+                        tasks.push(mailTransport.sendEmail(results['patronEmailOptions']));
+                        tasks.push(saveToLibInsight['']);
+                        // @TODO if the request required authentication, then update the status for the request to received?
+                        return Promise.all(tasks); */
+            return mailTransport.sendMail(results['libEmailOptions'])
+                .then(() => {
+                    console.log('Library notification sent/received request');
+
+                    //@TODO update request status for those requests that required authentication
+
+                    return mailTransport.sendMail(results['patronEmailOptions']);
+                })
+                .catch(error => {
+                    console.log(error);
+                    return Promise.reject(new Error(error));
+                });
         } else {
-            // return a Promis error?
-            return Promise.error()
+            // return a Promise error?
+            console.log(`Error sending notifications for request ${requestId}.`);
         }
     } else {
-        return Promise.error()
+        console.log(`Error preparing request ${requestId}.`);
     }
-
-    // Route request to appropriate Library resources.
-    //return mailTransport.sendEmail(libraryOptions)
-    //    .then(() => console.log('Message successfully sent.'))
-    //    .catch((error) => console.log('Error processing Library routing for request ' + requestId));
-
-    // Route request to appropriate Library resources.
-    /*  return mailTransport.sendEmail(libraryOptions, (error, info) => {
-        if (error) {
-          console.log('Error processing Library routing for request ' + requestId);
-        }
-        // Sends an email confirmation to the requestor.
-        return mailTransport.sendEmail(patronOptions, (error, info) => {
-          if (error) {
-            console.log('Error processing patron routing for request ' + requestId);
-          }
-          // @TODO Writes a copy of the request to Springshare.
-
-          // If an authenticated user, then update the status of the issue in the requestor's queue.
-          if (req.authenticated) {
-            let update = {};
-            update['/users/'+uid+'/requests/'+requestId+'/status'] = 'Received';
-            return firebase.database().ref().update(update);
-          }
-
-        });
-      });*/
-
+    return null;
 });
 
 function prepPurchaseRequestInfo(frmData, libOptions, userOptions) {
@@ -114,15 +106,15 @@ function prepPurchaseRequestInfo(frmData, libOptions, userOptions) {
     request['valid'] = true; // set after inputs validated and email body constructed
 
     // Prepare email content for Library staff
-    libOptions.from = req.fld_email_address;
-    libOptions.replyTo = req.fld_email_address;
+    libOptions.from = frmData.fldset_requestor_information.fld_email_address.value;
+    libOptions.replyTo = frmData.fldset_requestor_information.fld_email_address.value;
     // @TODO Routing varies based on format and if for reserves...
     libOptions.to = 'jlk4p@virginia.edu';
     libOptions.subject = 'Purchase Request';
     libOptions.text = 'This is where the fields and their values would be displayed...';
     request['libEmailOptions'] = libOptions;
     // Prepare email confirmation content for patron
-    userOptions.to = req.fld_email_address;
+    userOptions.to = frmData.fldset_requestor_information.fld_email_address.value;
     userOptions.subject = 'Purchase Request';
     userOptions.text = 'This is where the fields and their values would be displayed...';
     request['patronEmailOptions'] = userOptions;
@@ -137,20 +129,20 @@ function prepGovernmentInformationRequest(frmData, libOptions, userOptions) {
     request['valid'] = false;
 
     // Prepare email message body.
-    if (req.fld_uva_computing_id) {
-        inputs += "UVA Computing ID, e.g. mst3k: " + req.fld_uva_computing_id.value + "\n";
+    if (frmData.fld_uva_computing_id) {
+        inputs += "UVA Computing ID, e.g. mst3k: " + frmData.fld_uva_computing_id.value + "\n";
     }
-    if (req.fld_name.value && req.fld_email_address.value && req.fldset_question_or_comment.fld_enter_your_question_or_comment_regarding_governement_resourc.value) {
+    if (frmData.fld_name.value && frmData.fld_email_address.value && frmData.fldset_question_or_comment.fld_enter_your_question_or_comment_regarding_governement_resourc.value) {
         request['valid'] = true;
-        inputs += "Name: " + req.fld_name.value + "\n";
-        inputs += "Email address: " + req.fld_email_address.value + "\n";
-        inputs += "Question or Comment\n" + req.fldset_question_or_comment.fld_enter_your_question_or_comment_regarding_governement_resourc.value + "\n";
+        inputs += "Name: " + frmData.fld_name.value + "\n";
+        inputs += "Email address: " + frmData.fld_email_address.value + "\n";
+        inputs += "Question or Comment\n" + frmData.fldset_question_or_comment.fld_enter_your_question_or_comment_regarding_governement_resourc.value + "\n";
     }
 
     // Prepare email content for Library staff
     // @TODO Routing goes to Govtinfo address in production: govtinfo@groups.mail.virginia.edu
-    libOptions.from = req.fld_email_address.value;
-    libOptions.replyTo = req.fld_email_address.value;
+    libOptions.from = frmData.fld_email_address.value;
+    libOptions.replyTo = frmData.fld_email_address.value;
     libOptions.to = 'jlk4p@virginia.edu';
     libOptions.subject = 'Reference Referral';
     libOptions.text = "The question below was submitted through the Government Information Resources Contact Us page:\n\n";
@@ -159,7 +151,7 @@ function prepGovernmentInformationRequest(frmData, libOptions, userOptions) {
     // Prepare email confirmation content for patron
     userOptions.from = '"U.Va. Library: Govt. Info. Svcs." <govtinfo@groups.mail.virginia.edu>';
     userOptions.replyTo = '"U.Va. Library: Govt. Info. Svcs." <govtinfo@groups.mail.virginia.edu>';
-    userOptions.to = req.fld_email_address.value;
+    userOptions.to = frmData.fld_email_address.value;
     userOptions.subject = 'Your reference referral';
     userOptions.text = "Your request (copied below) has been received and will be referred to Government Information Resources.\n\n";
     userOptions.text += inputs;
