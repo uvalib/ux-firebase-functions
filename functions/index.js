5e16e419a2f94b6ebb51b5a53efd146919af1fa2
jlk4p <jlk4p@virginia.edu>
2019-04-02 11:21:35 -0400

    adjust other department input to only get populated if Other selected

--- functions/index.js
+++ functions/index.js
@@ -248,7 +248,7 @@ function processPurchaseRequest(reqId, submitted, frmData, libOptions, userOptio
         if (frmData.sect_requestor_information.fields.fld_lib_university_department_or_school.value) {
             requestorInfo += "<strong>" + frmData.sect_requestor_information.fields.fld_lib_university_department_or_school.label + ":</strong> " + frmData.sect_requestor_information.fields.fld_lib_university_department_or_school.value + "<br>\n";
             data['field_691'] = frmData.sect_requestor_information.fields.fld_lib_university_department_or_school.value;
-            if (frmData.sect_requestor_information.fields.fld_other_lib_department_school.value && frmData.sect_requestor_information.fields.fld_other_lib_department_school.value !== "") {
+            if (frmData.sect_requestor_information.fields.fld_lib_university_department_or_school.value === "Other...") {
                 requestorInfo += "<strong>" + frmData.sect_requestor_information.fields.fld_other_lib_department_school.label + ":</strong> " + frmData.sect_requestor_information.fields.fld_other_lib_department_school.value + "<br>\n";
                 data['field_751'] = frmData.sect_requestor_information.fields.fld_other_lib_department_school.value;
             }
