d3b82bc0a7c9228bf1315f67d39a696cfe48061f
Jack Kelly <jlk4p@virginia.edu>
2019-12-19 14:30:13 -0500

    remove binary modules not used

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,6 @@ const bucket = storage.bucket('uvalib-api.appspot.com');
 const request = require('request');
 const stripHtml = require('string-strip-html');
 const moment = require('moment');
-const base64 = require('base-64');
-const utf8 = require('utf8');
 
 // Environment variables configured for use with sending emails and saving data to LibInsight for forms.
 // See https://firebase.google.com/docs/functions/config-env
